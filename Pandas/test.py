ff004e5b9abd20537f07404902fc3f47c19ea8bf
wushuang5112 <xsx900101@gmail.com>
2018-06-03 15:12:25 +0800

    test

---
+++ Pandas/test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+PREVIOUS_MAX_ROWS = pd.options.display.max_rows
+pd.options.display.max_rows = 20
+np.random.seed(12345)
+import matplotlib.pyplot as plt
+import matplotlib
+plt.rc('figure', figsize=(10, 6))
+np.set_printoptions(precision=4, suppress=True)
+
+
